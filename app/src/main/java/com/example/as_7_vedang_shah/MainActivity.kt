0d012baf372e138106f289991faa99de32e890f7
vedangshah20 <w0845060@myscc.ca>
2025-03-29 20:17:42 -0400

    Completed As_7 With some help

--- app/src/main/java/com/example/as_7_vedang_shah/MainActivity.kt
+++ app/src/main/java/com/example/as_7_vedang_shah/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     //mutable list to add expense name and amount
     private val expensedatalist = mutableListOf<Expense>()
 
-    @SuppressLint("MissingInflatedId", "NotifyDataSetChanged")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
